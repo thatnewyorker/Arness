7f8f00d039ac0e86a5e4377dff472826242a8f7c
Gerard Cruzado <thatnewyorker@gmail.com>
2024-02-29 23:15:02 -0500

    Moved Cpu6502 struct definition to main.rs.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+mod cpu6502;    // Import the cpu module
+
+// Import the Cpu6502 struct from the cpu module
 fn main() {
-    println!("Hello, world!");
+    let mut cpu6502 = cpu6502::Cpu6502::new();
+
+    // Example usage: Load the value 0x10 into the accumulator
+    cpu6502.lda_immediate(0x10);
+    println!("Accumulator: {}", cpu6502.a);
 }
